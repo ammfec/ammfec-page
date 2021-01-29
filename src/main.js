ae2d491d439880fae0e48640088c33a3f89cd87e
Sandy Francisco <sandra.cortes@rappi.com>
2021-01-29 01:37:56 -0600

    Add form style

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './styles/global-styles.css'
-
+import VueToggles from 'vue-toggles';
+ 
 Vue.config.productionTip = false
+Vue.component('VueToggles', VueToggles);
 
 new Vue({
   render: h => h(App),
--- src/pages/container.vue
+++ src/pages/container.vue
@@ -16,28 +16,28 @@
                 <p class="subtitle-second">"Para nosotros la tecnología es una solución, no un obstáculo"</p>
                 <ul class="list-first">
                     <li>
-                        <img class="img-icon" src="./../assets/down-arrow.svg" />
+                        <img class="img-icon" src="./../assets/down-arrow.svg" v-on:click="showMision = !showMision" />
                         <p class="text-content">
                             MISIÓN 
-                            <span class="text-info">
+                            <span class="text-info" v-if="showMision">
                                 Ser una empresa socialmente responsable que contribuye con soluciones innovadoras
                             </span>
                         </p>    
                     </li>
                     <li>
-                        <img class="img-icon" src="./../assets/down-arrow.svg" />
+                        <img class="img-icon" src="./../assets/down-arrow.svg" v-on:click="showVision = !showVision" />
                         <p class="text-content">
                             VISIÓN 
-                            <span class="text-info">
+                            <span class="text-info" v-if="showVision">
                                 Ayudamos a comprender, utilizar y desarrollar herramientas tecnologicas que esten al avance de todos
                             </span>
                         </p>
                     </li>
                     <li>
-                        <img class="img-icon" src="./../assets/down-arrow.svg" />
+                        <img class="img-icon" src="./../assets/down-arrow.svg" v-on:click="showValues = !showValues" />
                         <p class="text-content">
                             NUESTROS VALORES
-                            <ul class="list-securities">
+                            <ul class="list-securities" v-if="showValues">
                                 <li class="list-value">Trabajo en equipo</li>
                                 <li class="list-value">Trabajo respetuoso</li>
                                 <li class="list-value">Innovación y competitividad</li>
@@ -50,6 +50,7 @@
                 </ul>
             </div>
         </div>
+
         <div class="section-we-make">
             <div class="container">
                 <div class="content">
@@ -68,12 +69,84 @@
         <div class="section-contact">
             <div class="container">
                 <div class="content">
-                    <h1 class="title">CONTACTANOS</h1>
+                    <h1 class="title-contact">CONTACTANOS</h1>
+                    <form style="margin-right: 105px">
+                    <input
+                        class="input-info"
+                        type="text"
+                        placeholder="Nombre" />
+                    <input
+                        class="input-info"
+                        type="text"
+                        placeholder="Apellido" />
+                    <input
+                        class="input-info"
+                        type="email"
+                        placeholder="Email"
+                        maxlength="10" />
+                    <input
+                        class="input-info"
+                        type="number"
+                        placeholder="Télefono" />
+                    <p>¿Qué tipo de proyecto estás buscando?</p>
+                    <div class="toogle-display">
+                            <VueToggles
+                                @click="value = !value"
+                                :value="value"
+                                checkedBg="#E9DBF5"
+                            />
+                        <p>Consultoria, asesoría y capacitación</p>
+                    </div>
+                    <div class="toogle-display">
+                        <VueToggles
+                            @click="value = !value"
+                            :value="value"
+                            checkedBg="#E9DBF5"
+                        />
+                        <p>Desarrollo de plataformas web</p>
+                    </div>
+                    <div class="toogle-display">
+                        <VueToggles
+                            @click="value = !value"
+                            :value="value"
+                            checkedBg="#E9DBF5"
+                        />
+                        <p>Aplicaciones móviles</p>
+                    </div>
+                    <div class="toogle-display">
+                        <VueToggles
+                            @click="value = !value"
+                            :value="value"
+                            checkedBg="#E9DBF5"
+                        />
+                        <p>Mantenimiento a proyectos ya existentes</p>
+                    </div>
+                    <input class="style-button" type="submit" />
+                </form>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: false,
+            showMision: false,
+            showVision: false,
+            showValues: false,
+        }
+    },
+
+    methods: {
+        functionShowMision() {
+            console.log("Hola")
+        }
+    }
+}
+</script>
 
 <style scoped>
 .hero {
@@ -161,7 +234,6 @@ ul .list-value {
 }
 .list-first li {
     display: flex;
-    flex-wrap: wrap;
     margin: 40px;
 }
 .section-we-make {
@@ -169,10 +241,6 @@ ul .list-value {
     background-repeat: no-repeat;
     background-size: cover;
     background-image: url("./../assets/background-1.png");
-}
-.subtitle-we-make {
-    font-size: 22px;
-    padding-bottom: 45px;
 }
 .section-we-make .container {
     padding: 12px 40px 0 0;
@@ -182,6 +250,10 @@ ul .list-value {
     height: 100vh;
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
+.subtitle-we-make {
+    font-size: 22px;
+    padding-bottom: 45px;
+}
 .section-contact {
     height: 100vh;
     background-repeat: no-repeat;
@@ -189,11 +261,48 @@ ul .list-value {
     background-image: url("./../assets/Fondo-contactanos.jpg");
 }
 .section-contact .container {
+    padding: 55px 0 0 1900px;
     display: grid;
-    height: 100vh;
+    float: right;
+    width: 40%;
+    height: 90vh;
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 .white-color {
     color: #FFFFFF
+}
+.title-contact {
+    font-size: 26px;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+.toogle-display {
+    display: flex;
+    padding: 10px;
+    width: 70%;
+}
+.toogle-display p {
+    padding-left: 10px;
+}
+.input-info {
+    width: 100%;
+    padding: 8px 20px;
+    margin: 8px 0;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    background: #ccc;
+}
+.style-button {
+    padding: 10px;
+    width: 62%;
+    background-color: #CBA4E5;
+    border: 1px #CBA4E5;
+    font-size: 15px;
+    font-weight: 600;
+    border-radius: 5px;
+}
+::placeholder {
+    color: black;
+    font-size: 15px;
 }
 </style>
